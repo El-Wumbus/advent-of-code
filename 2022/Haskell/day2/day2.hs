15d3728a66f11e1d82b6402d4a6a0115125bdbb1
c0ck <decator.c@proton.me>
2024-10-29 17:45:44 -0400

    Finish 2022|Day 2|Part 2 in Haskell

--- 2022/Haskell/day2/day2.hs
+++ 2022/Haskell/day2/day2.hs
@@ -7,6 +7,10 @@ scissors = 3
 rps_val :: [(Char, Int)]
 rps_val = [('A', rock), ('X', rock), ('B', paper),('Y', paper), ('C', scissors), ('Z', scissors)]
 
+-- The columns are space separated. Each consists of a single character.
+parseCols :: String -> (Char, Char)
+parseCols line = let cols = map (head) $ words line in (head cols, last cols)
+
 part1DecideWinner :: Int -> Int -> Int
 part1DecideWinner opp us 
     | opp == us = 3
@@ -15,22 +19,42 @@ part1DecideWinner opp us
         || opp == scissors && us == rock = 6
     | otherwise = 0
 
+
 part1SolveRound line =
-    if length line /= 0 then
-        us + part1DecideWinner opp us
-    else
-        0
+    if length line /= 0 then us + part1DecideWinner opp us
+    else 0
     where
-    cols = map (head) $ words line -- The columns are space separated. Each consists of a single character.
-    opp = fromJust $ lookup (head cols) rps_val
-    us = fromJust $ lookup (last cols) rps_val
+    (o, u) = parseCols line
+    opp = fromJust $ lookup o rps_val
+    us = fromJust $ lookup u rps_val
+
+part2DecideWinner (opp, outcome) =
+    roundScore + us
+    where
+    opponent = fromJust $ lookup opp rps_val
+    (roundScore, us) = case outcome of
+        'Y' -> (3, opponent)
+        'X' -> (0,
+            if opponent == rock then scissors
+            else if opponent == scissors then paper
+            else rock)
+        'Z' -> (6,
+            if opponent == rock then paper
+            else if opponent == scissors then rock
+            else scissors)
+
+part2SolveRound line =
+    if length line /= 0 then part2DecideWinner $ parseCols line
+    else 0
 
 solvePart1 input = foldl (+) 0 $ map part1SolveRound $ lines input
+solvePart2 input = foldl (+) 0 $ map part2SolveRound $ lines input
 
 main = do
     test_input <- readFile "test-input.txt"
     part1_input <- readFile "part1-input.txt"
-
-    print $ solvePart1 test_input
-    print $ solvePart1 part1_input
-    
+    part2_input <- readFile "part2-input.txt"
+    putStrLn $ "Part 1 test: " ++ (show $ solvePart1 test_input)
+    putStrLn $ "Part 1: " ++ (show $ solvePart1 part1_input)
+    putStrLn $ "Part 2 test: " ++ (show $ solvePart2 test_input)
+    putStrLn $ "Part 2: " ++ (show $ solvePart2 part2_input)
