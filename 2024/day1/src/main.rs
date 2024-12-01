48d7bca8e9dcb9a18dd7f1ef87ba51d7155e7611
Decator <decator.c@proton.me>
2024-12-01 11:32:48 -0500

    Solve AoC 2024 Day 1 - part 2 in Rust

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -10,16 +10,40 @@ fn part1() -> i32 {
             let mut pairs = line.split_whitespace();
             (pairs.next().unwrap(), pairs.next().unwrap())
         })
+        .map(|(l, r)| (l.parse::<i32>().unwrap(), r.parse::<i32>().unwrap()))
         .unzip();
     left.sort();
     right.sort();
 
     left.into_iter()
         .zip(right.into_iter())
-        .map(|(l, r)| (l.parse::<i32>().unwrap(), r.parse::<i32>().unwrap()))
         .fold(0, |acc, (l, r)| acc + i32::abs(l - r))
 }
 
+fn part2() -> i32 {
+    let (left, right): (Vec<_>, Vec<_>) = INPUT
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|line| {
+            // I split them like this because I'm not sure of what kind of
+            // whitespace they're separated by.
+            let mut pairs = line.split_whitespace();
+            (pairs.next().unwrap(), pairs.next().unwrap())
+        })
+        .map(|(l, r)| (l.parse::<i32>().unwrap(), r.parse::<i32>().unwrap()))
+        .unzip();
+
+    let right = right.iter();
+    left.into_iter()
+        .map(|n| n * right.clone().filter(|r| **r == n).count() as i32)
+        .sum()
+}
+
 fn main() {
-    println!("PART 1: {}", part1());
+    let part1 = part1();
+    let part2 = part2();
+    println!("PART 1: {part1}");
+    println!("PART 2: {part2} ");
+    assert_eq!(part2, 26800609);
+    assert_eq!(part1, 1530215);
 }
